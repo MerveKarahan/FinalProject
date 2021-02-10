2da47bea02b935d2911b91f2f6889d1c4c66a000
merve karahan <betulmervekarahan@gmail.com>
2021-02-10 23:24:13 +0200

    10.02.2021

---
+++ Business/Concrete/SuccessDataResult.cs
@@ -0,0 +1,27 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class SuccessDataResult<T>:DataResult<T>
+    {
+        public SuccessDataResult(T data, string message) : base(data, true, message)
+        {
+
+        }
+        public SuccessDataResult(T data):base(data,true)
+        {
+
+        }
+        public SuccessDataResult(string message):base(default,true,message)
+        {
+
+        }
+        public SuccessDataResult(List<Entities.DTOs.ProductDetailDto> lists) :base(default,true)
+        {
+
+        }
+    }
+}
